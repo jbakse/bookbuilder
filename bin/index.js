df629ebc9e6be0f44071ff2d0d1a3ecde4195543
Justin Bakse <justin@tsfim.com>
2020-12-30 14:53:53 -0600

    read md, convert, write html

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
+const bookbuilder = require("../lib/index");
 console.log("Hello, Bookbuilder!");
+
+let fs = require("fs");
+
+fs.readFile("../compformnet/src/index.md", function (err, data) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  let markdown_source = data.toString("utf8");
+
+  console.log(markdown_source);
+
+  let html = bookbuilder.convert(markdown_source);
+  console.log(html);
+
+  fs.writeFile("../compformnet/dist/index.html", html, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+});
---
+++ notes/notes.md
@@ -0,0 +1,1 @@
+https://medium.com/jspoint/creating-cli-executable-global-npm-module-5ef734febe32
