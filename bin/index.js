a72d93be4c63e15fa40385cfb199e2da5a1937fd
Justin Bakse <justin@tsfim.com>
2020-12-30 20:15:28 -0600

    add support for multiple md files

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// var glob = require("glob");
+var glob = require("glob");
 
 const fs = require("fs");
 const { promisify } = require("util");
@@ -18,17 +18,13 @@ async function writeFile(path, data) {
   await fs_writeFile(path, data);
 }
 
-async function main() {
-  console.log("Hello, Bookbuilder!!");
-
-  const source_path = "./content/index.md";
-  const dest_path = "./dist/index.html";
+async function handleMarkdownFile(src, dest) {
   let source_markdown;
 
   try {
-    source_markdown = await readFile(source_path);
+    source_markdown = await readFile(src);
   } catch (err) {
-    console.log(`An error occurred reading file '${source_path}'`);
+    console.log(`An error occurred reading file '${src}'`);
     console.log(err);
     return;
   }
@@ -36,11 +32,36 @@ async function main() {
   const html = bookbuilder.convert(source_markdown);
 
   try {
-    await writeFile(dest_path, html);
+    await writeFile(dest, html);
   } catch (err) {
-    console.log(`An error occured writing file '${dest_path}'`);
+    console.log(`An error occured writing file '${dest}'`);
     console.log(err);
   }
+}
+
+async function handleMarkdownFiles(err, srcs) {
+  for (const src of srcs) {
+    let dest = src;
+    // change directory
+    let dest_parts = dest.split("/");
+    dest_parts[0] = "dist";
+    dest = dest_parts.join("/");
+    // change extension
+    dest = dest.substr(0, dest.lastIndexOf(".")) + ".html";
+
+    console.log(`Handling Markdown: ${src} -> ${dest}`);
+    await handleMarkdownFile(src, dest);
+  }
+}
+
+async function main() {
+  console.log("Hello, Bookbuilder!!!");
+
+  // const source_path = "./content/index.md";
+  // const dest_path = "./dist/index.html";
+  // let source_markdown;
+
+  glob("content/**/*.md", false, handleMarkdownFiles);
 }
 
 main();
